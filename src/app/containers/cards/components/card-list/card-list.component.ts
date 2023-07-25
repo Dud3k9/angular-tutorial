b38fe36a2fbb99019efe67b2e0c1c1f8595e9509
Maciej Petrykowski <maciej.petrykowski@itboom.pl>
2023-07-25 16:59:31 +0200

    declarative cards

--- src/app/containers/cards/components/card-list/card-list.component.ts
+++ src/app/containers/cards/components/card-list/card-list.component.ts
@@ -1,19 +1,38 @@
 import { Component, OnInit } from "@angular/core";
 import { CardService } from "../../services/card.service";
-import { Observable } from "rxjs";
+import { BehaviorSubject, Observable, Subject, switchMap, tap } from "rxjs";
 import { Card } from "../../models/card.model";
+import { takeUntilDestroyed } from "@angular/core/rxjs-interop";
 
 @Component({
   selector: "app-card-list",
   templateUrl: "./card-list.component.html",
   styleUrls: ["./card-list.component.css"],
 })
-export class CardListComponent implements OnInit {
-  cards$: Observable<Card[]>;
+export class CardListComponent {
+  //data
+  cards$: Subject<Card[]> = new Subject();
+
+  //triggers
+  private refreshCards$ = new BehaviorSubject<void>(undefined);
+  addCard$ = new Subject<Card>();
 
   constructor(private cardService: CardService) {
-    this.cards$ = this.cardService.getCards();
-  }
+    //events
+    this.refreshCards$
+      .pipe(
+        takeUntilDestroyed(),
+        switchMap(() => this.cardService.getCards()),
+        tap((newCards) => this.cards$.next(newCards))
+      )
+      .subscribe();
 
-  ngOnInit() {}
+    this.addCard$
+      .pipe(
+        takeUntilDestroyed(),
+        switchMap((newCard) => this.cardService.createCard(newCard)),
+        tap(() => this.refreshCards$.next())
+      )
+      .subscribe();
+  }
 }
